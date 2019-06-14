486124fc29fbf103f43e0a7565185bfad98b34aa
Mike Lofgren <mikeylofgren@gmail.com>
2019-06-14 12:46:59 -0700

    for loop iteration in progress.

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,41 @@ $(document).ready(function(){
     var userNumber = parseInt($("input#input").val());
     // countArray.push(userNumber)
     roboCount(userNumber);
-    roboCheck(newArray);
+    var endResult = roboCheck(newArray);
+     $("#output").append(newArray + "<br>");
   })
 })
 
 
 
-newArray = []
+newArray = [ ]
+finalArray = []
 function roboCount(userNumber) {
 
-  for (var i=0; i < userNumber; i++) {
+  for (var i=1; i <=userNumber; i++) {
        newArray.push(i)
       // console.log(i + userNumber)
        console.log(newArray)
     }
-  };
+};
+
+
+function roboCheck(newArray) {
+
+console.log("working")
+  for (var i=0; i < newArray.length; i++) {
+      if (newArray[i] === 1) {
+        newArray[i] = (" Beep")
+      } else if (newArray[i] === 2) {
+        newArray[i] = (" Boop!")
+      } else if (newArray[i] === 3) {
+        newArray[i] = (" I'm sorry, Dave. I'm afraid I can't do that.")
+      }
+      //  finalArray.push(i)
+      // console.log(i + userNumber)
+      //  console.log(newArray)
+    }
+};
 
 
     // if (userNumber[i] === NaN) {
