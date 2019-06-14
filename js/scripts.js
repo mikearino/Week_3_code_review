8671864c752e3b62f7e5f9ecebef6d8bbe9a4f16
Mike Lofgren <mikeylofgren@gmail.com>
2019-06-14 10:02:28 -0700

    create for loop that counts numbers and pushes to array.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,26 @@ $(document).ready(function(){
   $("form#formOne").submit(function(event){
     event.preventDefault();
     var userNumber = parseInt($("input#input").val());
-    console.log(userNumber)
-    var runBot = roboCount(userNumber);
+    // countArray.push(userNumber)
+    roboCount(userNumber);
+    roboCheck(newArray);
   })
 })
+
+
+
+newArray = []
+function roboCount(userNumber) {
+
+  for (var i=0; i < userNumber; i++) {
+       newArray.push(i)
+      // console.log(i + userNumber)
+       console.log(newArray)
+    }
+  };
+
+
+    // if (userNumber[i] === NaN) {
+    //   return
+    // } else if (userNumber[i] === ) {}
+  // }
