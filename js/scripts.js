bce997a8a0920984512643bea362624ccf0bf393
Mike Lofgren <mikeylofgren@gmail.com>
2019-06-21 20:09:29 -0700

    clean up code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,25 @@
-$(document).ready(function() {
-  // console.log("jQuery is working!")
-  $('form#formOne').submit(function(event) {
+$(document).ready(function () {
+  $('form#formOne').submit(function (event) {
     event.preventDefault();
-    var userNumber = parseInt($('input#input').val());//33
-    var returnedArray =  roboCount(userNumber);//[0...33]
+    var userNumber = parseInt($('input#input').val());
+    var returnedArray =  roboCount(userNumber);
     var countForBoops = roboCheck(returnedArray);
-    console.log(countForBoops)
-
-
-
+    console.log(countForBoops);
     $('#output').text(countForBoops);
   });
 
   function roboCount(num) {
     var newArray = [];
-
-
     for (var i = 0; i <= num; i++) {
       newArray.push(i);
-    }
+    };
 
     return newArray;
   };
+
   function roboCheck(arrayOfNumbers) {
-
-
-    // console.log("working")
     for (var y = 0; y < arrayOfNumbers.length; y++) {
-      //newArray[i]=12
-      // var stringNow = newArray[i].toString()
-      // var arrayIntoString = input.toString()
-      // var stringNow = [i].length.toString()
       var numberString = arrayOfNumbers[y].toString();
-      
       if (numberString.includes(3)) {
         arrayOfNumbers[y] = "That's gonna be a no for me dave"
       } else if (numberString.includes(2)) {
@@ -41,44 +28,6 @@ $(document).ready(function() {
         arrayOfNumbers[y] = "beep";
       }
     }
-    return arrayOfNumbers
+    return arrayOfNumbers;
   }
-  //newArray[j]=1
-  //newArray[j]=2
-
-  // else if (newArray[j] === 2 ){
-  //   newArray[i] = (" Boop!")
-  // } else if (newArray[j] === 3 ){
-  //   newArray[i] = (" I'm sorry, Dave. I'm afraid I can't do that.")
-  // } else if (newArray[j] >= 100) {
-  //   newArray[i] = (" Funputers batteries are low")
-  // return input;
-
-  //       }
-  //     }
-  // //   }
-  // // };
-
-  // var experiment = roboCheck([12, 123, 456, 444]);
-  // console.log(experiment);
-  //   }
-  // }
 });
-
-
-
-
-
-
-
-
-// // console.log("working")
-// for (var i=0; i < newArray.length; i++) {
-  //   //newArray[i]=12
-  //   var numLength = newArray[i].toString().length;
-  //
-  //   for(var j=0; j< numLength; j++){
-    //     var num = newArray[i]
-    //     var digit = num[j]
-    //   //newArray[j]=1
-    //   //newArray[j]=2
